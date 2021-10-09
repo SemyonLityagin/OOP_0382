8939d52f5a935027d4a6bda48763b97be569334e
Fittern <46086112+Fittern@users.noreply.github.com>
2021-10-09 07:53:05 +0300

    Update Floor.cpp

--- Ilyin/Game/Floor/Floor.cpp
+++ Ilyin/Game/Floor/Floor.cpp
@@ -1,7 +1,5 @@
 #include "Floor.h"
 
-Floor::Floor() {}
-
 Floor::Floor(int width, int height): width(width), height(height) {
     cells = new Cell**[WIDTH_OF_FLOOR];
     for (int i = 0; i < WIDTH_OF_FLOOR; i++) cells[i] = new Cell*[HEIGTH_OF_FLOOR];
@@ -21,6 +19,19 @@ Floor& Floor::operator=(const Floor& other) {
     }
     return *this;
 }
+
+Floor& Floor::operator=(Floor&& other){
+    this->width = other.width;
+    other.width = 0;
+    this->height = other.height;
+    other.height = 0;
+    for(int i = 0; i < WIDTH_OF_FLOOR; i++) {
+        for (int j = 0; j < HEIGTH_OF_FLOOR; j++) {
+            this->cells[i][j] = other.cells[i][j];
+        }
+    }
+    return *this;
+};
 
 void Floor::Draw_Floor(sf::RenderWindow* window) {
     for (int i = 0; i < width; ++i) {
